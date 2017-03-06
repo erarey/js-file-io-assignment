db032357689f088b134598acd65fd9acfc1082d7
Eli Rarey <erarey@gmail.com>
2017-03-06 16:18:40 -0600

    Altered Employee.js to not use static keyword, for practice
    altered Employee.js to not use static keyword, for practice

--- Employee.js
+++ Employee.js
@@ -3,27 +3,34 @@
 const fs = require('fs')
 
 class Employee {
+
   constructor (name, title, salary) {
     this.name = name
     this.title = title
     this.salary = salary
   };
 
+  /*
   static parseFromFilePath (path) {
     let content
     content = fs.readFileSync(path)
     const parsedData = JSON.parse(content)
     return new Employee(parsedData.name, parsedData.title, parsedData.salary)
   };
+  */
 
   promote (title, salary) {
     this.title = title
     this.salary = salary
   };
-  // TODO ???
 }
 
-// todo
+Employee.parseFromFilePath = (path) => {
+  let content
+  content = fs.readFileSync(path)
+  const parsedData = JSON.parse(content)
+  return new Employee(parsedData.name, parsedData.title, parsedData.salary)
+}
 
 module.exports = {
   Employee
